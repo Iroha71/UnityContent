eb434b126f961a935a473fe67532037f6b289778
iroha <y.koba.h1071@outlook.jp>
2025-06-21 00:01:57 +0900

    記事をコンテンツコレクションとして管理するように変更

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const postCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    thumbnail: image(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  invector: postCollection,
+}
