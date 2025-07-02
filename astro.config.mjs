02e2384141acc3fd56ad77de8d8f7fa23d0142aa
iroha <y.koba.h1071@outlook.jp>
2025-07-02 10:08:43 +0900

    mermaid適用

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import dotenv from 'dotenv';
 import vue from '@astrojs/vue';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import rehypeMermaid from 'rehype-mermaid';
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iroha71.github.io',
   base: 'UnityContent',
-  integrations: [vue(), react(), mdx()]
+  integrations: [vue(), react(), mdx()],
+  markdown: {
+    syntaxHighlight: {
+      excludeLangs: ["mermaid"],
+    },
+    rehypePlugins: [rehypeMermaid],
+  }
 });
